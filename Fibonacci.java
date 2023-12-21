548fffd84f1967872f84737d39dce2919677109a
hakaishin <study123lad@gmail.com>
2023-12-21 17:40:22 -0500

    first commit

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+package basic_problems;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n1=0,n2=1,n;
+        
+        System.out.print("enter the values of n:");
+        Scanner inputScanner=new Scanner(System.in);
+        n=inputScanner.nextInt();
+        System.out.print(n1+" "+n2+" ");
+        for(int i=1;i<=n-2;i++){
+            int n3=n1+n2;
+            System.out.print(n3+" ");
+            n1=n2;
+            n2=n3;
+        }
+        inputScanner.close();
+    }
+}
---
+++ PrimeOrNot.java
@@ -0,0 +1,25 @@
+package basic_problems;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        int number,factor=0;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("enter the input:");
+        number=input.nextInt();
+
+        for(int i=1;i<=number;i++){
+            if(number%i==0)
+                factor++;
+        }
+
+        if(factor==2)
+            System.out.println("Given number "+number+" is prime!");
+        else
+            System.out.println("Given number "+number+" is not prime!");
+        
+        input.close();
+    }
+}
